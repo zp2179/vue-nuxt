6eda259e3b2a14ba591c44c759df788b736c69eb
zhupeng <zp2179@163.com>
2019-07-25 18:05:43 +0800

    init

---
+++ pages/test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>asdfasdf</div>
+</template>
+
+<script>
+    export default {
+        name: "test"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>user123123</div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
